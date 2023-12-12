9429fd8bdd5206c842b73147b348d8265c05b7a7
Your Name <Muhammad Huzaifa Rauf>
2023-12-12 23:05:35 +0500

    first-commit of next.js

---
+++ app/(components)/card/card.js
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div>
+                <h1>
+            Card
+        </h1>
+        <p>Your Card will appear here</p>
+        </div>    
+    )
+}
+export default Card;
---
+++ app/services/page.js
@@ -0,0 +1,15 @@
+import Card from "../(components)/card/card";
+
+function Services(){
+    return(
+        <div>
+            <h1>
+                Servives
+            </h1>
+            <p>Welcome to our Services Page</p>
+            <Card />
+        </div>
+        
+    )
+}
+export default Services;
